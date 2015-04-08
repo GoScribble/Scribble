6705d48f05d5e62ac36001a096660d9afef7af83
jamesk322 <james.kirkwood322@googlemail.com>
2015-04-08 23:27:33 +0200

    Added creation of dummy config file

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,40 @@ require "vendor/autoload.php";
 
 //Rename the config file
 rename("Config/config.example.php", "Config/config.php");
+
+//Fill the config file with dummy (testable) config settings
+$config = '<?php
+
+return [
+
+    "Providers" => [
+        
+        [
+        
+            "name"              => "Wordpress",
+            "active"            => true,
+            "nickname"          => "wp",
+            "url"               => "http://localhost",
+            "username"          => "user",
+            "password"          => "pass",
+            "provider_class"    => "Wordpress"
+        
+        ]
+    
+    ],
+    
+    "Groups" => [
+    
+        "ExGroup" => [
+        
+            "wp"
+            
+        ]
+        
+    ]
+
+]
+
+?>';
+
+file_put_contents("Config/config.php", $config);
