f1a92a58f5ff73ad8efb81db173bf3e0f2aa0d35
jamesk322 <james.kirkwood322@googlemail.com>
2015-04-09 01:51:58 +0200

    Fixed all issues

--- tests/PublisherServiceTest.php
+++ tests/PublisherServiceTest.php
@@ -7,6 +7,18 @@ class PublisherServiceTest extends \PHPUnit_Framework_TestCase
     public function testOnlyDefinition()
     {
         $service = new PublisherService();
-        $this->assertFalse($service->only(["wpp"]));
+        $this->assertNotFalse($service->only(["wp"]));
+    }
+    
+    public function testAllDefinition()
+    {
+        $service = new PublisherService();
+        $this->assertNotFalse($service->all());
+    }
+    
+    public function testOnlyDefinitionWithInvalidNickname()
+    {
+        $service = new PublisherService();
+        $this->assertFalse($service->only(["wp", "NicknameThatDoesntExist"]));
     }
 }
