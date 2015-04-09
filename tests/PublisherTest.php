03d5ca2c53eac4456853bcf31c6fb28fbeb82e1f
jamesk322 <james.kirkwood322@googlemail.com>
2015-04-09 10:58:21 +0200

    Removed all tests

--- tests/PublisherTest.php
+++ tests/PublisherTest.php
@@ -1,22 +1,8 @@
-<?php /*
+<?php
 
 use Scribble\Publisher;
 
 class PublisherTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAllDefinition()
-    {
-        $this->assertInstanceOf("\Scribble\PublisherService", Publisher::all());
-    }
     
-    public function testOnlyDefinition()
-    {
-        $this->assertInstanceOf("\Scribble\PublisherService", Publisher::only(["wpp"]));
-    }
-    
-    public function testOnlyDefinitionWithInvalidNicknames()
-    {
-        $this->expectOutputString("Scribble Exception: There are no providers available for use, check your Scribble 'Config/config.php' file");
-        Publisher::only(["wp", "SomeNonExistantNickname"]);
-    }
-}*/
+}
